3ae265ec562796c179da1a838c6667a6c45be0ae
Akarshan Gnaneswaran <75044525+Aka3D@users.noreply.github.com>
2022-08-05 16:22:35 +0200

    Validation-Improvement

--- HelloWorld/Model/UserModel.cs
+++ HelloWorld/Model/UserModel.cs
@@ -7,12 +7,16 @@ namespace HelloWorld.Model
     public class UserModel
     {
         //Register
+        [Required]
         public string Anrede1 { get; set; }
         [Required]
+        [RegularExpression(@"^[a-zA-Z]+$")]
         public string Vorname1 { get; set; }
         [Required]
+        [RegularExpression(@"^[a-zA-Z]+$")]
         public string Nachname1 { get; set; }
-        [Required] 
+        [Required]
+        [EmailAddress]
         public string Email1 { get; set; }
         [Required]
         [StringLength(50, MinimumLength = 8)]
@@ -23,6 +27,7 @@ namespace HelloWorld.Model
         [Range(1, 9999)]
         public string PLZ1 { get; set; }
         [Required]
+        [RegularExpression(@"^[a-zA-Z]+$")]
         public string Ort1 { get; set; }
         [Required]
         public string Land1 { get; set; }
@@ -30,15 +35,19 @@ namespace HelloWorld.Model
 
         //Zusatz-Adresse
         public string Anrede2 { get; set; }
+        [RegularExpression(@"^[a-zA-Z]+$")]
         public string Vorname2 { get; set; }
+        [RegularExpression(@"^[a-zA-Z]+$")]
         public string Nachname2 { get; set; }
         public string Strasse2 { get; set; }
         public string PLZ2 { get; set; }
+        [RegularExpression(@"^[a-zA-Z]+$")]
         public string Ort2 { get; set; }
         public string Land2 { get; set; }
 
         //Login
         [Required]
+        [EmailAddress]
         public string Email2 { get; set; }
         [Required]
         [StringLength(50, MinimumLength = 8)]
